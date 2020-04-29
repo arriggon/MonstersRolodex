74491bf9461d9aa79eb47cdb59e2a9c0b8e5df88
Moritz Brandstätter <arriggon@gmail.com>
2020-04-30 01:13:57 +0200

    Updates

---
+++ precache-manifest.4418db8b602df24a4a9acb7bde2badda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7671a3973b39f1091673ec9e30f4e82",
+    "url": "/ReactUdemyGuide/index.html"
+  },
+  {
+    "revision": "c5b73dcb07b887ee4025",
+    "url": "/ReactUdemyGuide/static/css/main.d52cd63c.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/ReactUdemyGuide/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ReactUdemyGuide/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c5b73dcb07b887ee4025",
+    "url": "/ReactUdemyGuide/static/js/main.6c5c5c8c.chunk.js"
+  },
+  {
+    "revision": "d7f52102a17df954cf03",
+    "url": "/ReactUdemyGuide/static/js/runtime-main.2d92ea0d.js"
+  }
+]);
